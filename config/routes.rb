f25b8875d12985df36a4c3cae0ab7eeccf2ace9d
Yishen Chang <samkami@icloud.com>
2015-10-24 20:37:57 +0800

    Add about page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
+  get '/about' => 'welcome#about'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
